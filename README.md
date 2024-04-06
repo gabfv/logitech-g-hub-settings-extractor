28de2344f74a034cd7d3d24664e4c8f5e907c8a7
gabfv <github_public@gabfv.net>
2024-04-06 11:33:55 -0400

    Now accepting script parameters - The path to the settings.db can be set and the path to folder of settings can be set too. - Unsupported platforms can now run but the settings folder path must be set with --settings_path - The user must now press confirm to continue after editing the file and closing the editor. - Various refactors

--- README.md
+++ README.md
@@ -17,6 +17,21 @@ It extracts the JSON. It asks you to close Logitech G Hub then, once it's done, 
 The script open it into your editor (the default app per OS settings for json files, or the OS will let ask you what to open it with). You edit the file as you like. Then, you save the file and close the editor.
 As soon as you close the editor, it automatically insert the modified JSON into Logitech G Hub sqlite db. When you open Logitech G Hub again, it should reflects the changes you've made.
 
+## Script parameters (all are optional)
+
+```
+usage: G Hub Settings Extractor [-h] [-d DB_PATH] [-s SETTINGS_PATH]
+
+Extract and replace the settings.json inside the settings.db used by Logitech G Hub.
+
+options:
+  -h, --help            show this help message and exit
+  -d DB_PATH, --db_path DB_PATH
+                        Path to an alternate settings.db file to edit. The result will not be written back to this file but in the default LGHUB settings folder (unless one has been provided with -s(--settings_path)).
+  -s SETTINGS_PATH, --settings_path SETTINGS_PATH
+                        Path to the LGHUB settings folder
+```
+
 ### Windows
 
 In a cmd or PowerShell window, type the following in the same folder as the py file.
--- ghub-settings.py
+++ ghub-settings.py
@@ -8,6 +8,7 @@ import os
 import sys
 import shutil
 import sqlite3
+import argparse
 
 DEFAULT_FOLDER_LG_GHUB_SETTINGS = None
 
@@ -16,17 +17,22 @@ if sys.platform.startswith('win'): # Windows
 elif sys.platform.startswith('darwin'): # MacOS
     DEFAULT_FOLDER_LG_GHUB_SETTINGS = os.path.expandvars('$HOME/Library/Application Support/lghub/') # Must end with /
 else:
-    error_message = """
-ERROR: Unsupported platform
+    warning_message = """
+WARNING: Unsupported platform! Make sure the script parameter -s (--settings_path) is set to the correct path for the LGHUB settings folder.
 {platform}
     """
-    print(error_message.format(platform=sys.platform))
-    exit(1)
+    print(warning_message.format(platform=sys.platform))
 
 DEFAULT_FILENAME_SETTINGS_DB = 'settings.db'
 DEFAULT_FILENAME_SETTINGS_JSON = 'EDIT_ME.json'
 DEFAULT_PATH_SETTINGS_DB = DEFAULT_FOLDER_LG_GHUB_SETTINGS + DEFAULT_FILENAME_SETTINGS_DB
 
+parser = argparse.ArgumentParser(
+    prog='G Hub Settings Extractor',
+    description='Extract and replace the settings.json inside the settings.db used by Logitech G Hub.',
+    epilog='See https://github.com/gabfv/logitech-g-hub-settings-extractor',)
+parser.add_argument('-d', '--db_path', type=str, default=DEFAULT_PATH_SETTINGS_DB, help='Path to an alternate settings.db file to edit. The result will not be written back to this file but in the default LGHUB settings folder (unless one has been provided with -s(--settings_path)).')
+parser.add_argument('-s', '--settings_path', type=str, default=DEFAULT_FOLDER_LG_GHUB_SETTINGS, help='Path to the LGHUB settings folder')
 
 def make_backup(file_path):
     backup_file_path = file_path + datetime.datetime.now().strftime('.%Y-%m-%d_%H-%M-%S')
@@ -92,16 +98,16 @@ Error:
         print(error_message.format(file_path=file_path, exception_message=error))
 
 
-def read_blob_data(data_id, file_path):
+def read_blob_data(data_id, args):
     sqlite_connection = 0
     try:
-        sqlite_connection = sqlite3.connect(file_path)
+        sqlite_connection = sqlite3.connect(args.db_path)
         cursor = sqlite_connection.cursor()
 
         sql_fetch_blob_query = """SELECT _id, FILE from DATA where _id = ?"""
         cursor.execute(sql_fetch_blob_query, (data_id,))
         record = cursor.fetchall()
-        settings_file_path = DEFAULT_FOLDER_LG_GHUB_SETTINGS + DEFAULT_FILENAME_SETTINGS_JSON
+        settings_file_path = args.settings_path + DEFAULT_FILENAME_SETTINGS_JSON
         for row in record:
             print("Id = ", row[0])
             settings_data = row[1]
@@ -153,15 +159,21 @@ def insert_blob(data_id, updated_settings_file_path, db_file_path):
         if sqlite_connection:
             sqlite_connection.close()
 
+def ask_to_continue():
+    if sys.version_info[0] < 3:
+        raw_input()
+    else:
+        input()
 
 if __name__ == '__main__':
-    if not os.path.exists(DEFAULT_PATH_SETTINGS_DB):
+    args = parser.parse_args()
+    if not os.path.exists(args.db_path):
         failure_to_find_settings_db = """
 ERROR: The file settings.db was not found! The path below was checked:
 {path}
 Quitting...
         """
-        print(failure_to_find_settings_db.format(path=DEFAULT_PATH_SETTINGS_DB))
+        print(failure_to_find_settings_db.format(path=args.db_path))
         exit(10)
 
     program_introduction_notification = """
@@ -170,27 +182,27 @@ This program is intended to extract and replace the settings.json inside the set
 Press Enter to continue.
     """
     print(program_introduction_notification)
-    if sys.version_info[0] < 3:
-        raw_input()
-    else:
-        input()
+    ask_to_continue()
+
     print("This program will extract the settings from the database...")
-    latest_id = get_latest_id(DEFAULT_PATH_SETTINGS_DB)
-    file_written = read_blob_data(latest_id, DEFAULT_PATH_SETTINGS_DB)
-    make_backup(DEFAULT_PATH_SETTINGS_DB)
+    latest_id = get_latest_id(args.db_path)
+    file_user_edit = read_blob_data(latest_id, args)
+    make_backup(args.db_path)
     print("IMPORTANT: PLEASE CLOSE LG G HUB NOW")
     print("The extracted file will be open after you press Enter.")
     print("Please edit it and don't forget to save the file then close the file (and the program that opened with)")
-    if sys.version_info[0] < 3:
-        raw_input()
-    else:
-        input()
+    ask_to_continue()
+    
     if sys.platform.startswith('win'): # Windows
-        os.system(file_written)
+        os.system(file_user_edit)
     elif sys.platform.startswith('darwin'): # MacOS
-        os.system('open "' + file_written + '"')
+        os.system('open "' + file_user_edit + '"')
     else:
-        print(file_written)
-    insert_blob(latest_id, file_written, DEFAULT_PATH_SETTINGS_DB)
+        print(file_user_edit)
+    # os.system won't necessarily wait for the editor to close.
+    print("Press Enter when you have saved the file and closed the editor.")
+    ask_to_continue()
+
+    insert_blob(latest_id, file_user_edit, args.db_path)
     print("The settings have been updated.")
     exit(0)
